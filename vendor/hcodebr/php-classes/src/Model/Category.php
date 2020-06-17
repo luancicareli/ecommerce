2540d5cafcc30f11462eeb82f94c90ca9252f39a
luancicareli <luancicarelli3@gmail.com>
2020-06-17 12:25:41 -0300

    categories views

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -20,7 +20,8 @@ class Category extends Model{
             ":idcategory"=>$this->getidcategory(),
             ":descategory"=>$this->getdescategory(),
         ));
-       $this->setData($results[0]);
+        $this->setData($results[0]);
+        Category::updateFile();
     }
     public function get($idcategory){
 
@@ -36,6 +37,16 @@ class Category extends Model{
         $sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory",[
             ':idcategory'=>$this->getidcategory()
         ]);
+        Category::updateFile();
+    }
+    public static function updateFile(){
+
+        $categories = Category::listAll();
+        $html =[];
+        foreach ($categories as $row) {
+            array_push($html,'<li><a href="/categories/' . $row['idcategory'] . '">' . $row['descategory'] . '</a></li>');
+        }
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html",implode('',$html));
     }
 }
 ?>
