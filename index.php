4378a2686d57a34e1a89802fbc33dcfb445f9f2e
luancicareli <luancicarelli3@gmail.com>
2020-06-18 13:23:26 -0300

    products site

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ $app = new Slim();
 $app->config('debug', true);
 
 require_once("admin-login.php");
+require_once("functions.php");
 require_once("admin-users.php");
 require_once("admin-categories.php");
 require_once("admin-products.php");
--- site.php
+++ site.php
@@ -1,12 +1,17 @@
 <?php
 
 Use \Hcode\Page;
+Use \Hcode\Model\Product;
 
 $app->get('/', function() {
 
+    $products = Product::listAll();
+
     $page = new Page();
 
-    $page->setTpl("index");
+    $page->setTpl("index",[
+        'products'=>Product::checkList($products)
+    ]);
 
 });
 
